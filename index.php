f7a61a400558b004945aeca8c6805ba988d3c947
codekissyoung <1162097842@qq.com>
2019-12-14 14:19:42 +0800

    开始研究 TP 框架

--- index.php
+++ index.php
@@ -1,26 +1,12 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
+if(version_compare(PHP_VERSION,'5.3.0','<'))
+    die('require PHP > 5.3.0 !');
 
-// 应用入口文件
-
-// 检测PHP环境
-if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
-
-// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG',True);
-
-// 定义应用目录
-define('APP_PATH','./Application/');
-
-// 引入ThinkPHP入口文件
+// THINK_PATH	框架目录
+// STORAGE_TYPE	存储类型（默认为File）
+// APP_MODE	应用模式（默认为common）
+define('BUILD_DIR_SECURE', false);          # don't generate index.html
+define('APP_DEBUG', TRUE);                  # debug mode
+define('APP_PATH','./Application/');        # apps dir, only one
+define('RUNTIME_PATH', '/tmp/tp-runtime');  # runtime dir, need writable   
 require './ThinkPHP/ThinkPHP.php';
-
-// 亲^_^ 后面不需要任何代码了 就是如此简单
