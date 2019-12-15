57bd7af26fcd01033082b1efc746b37b179a1571
codekissyoung <1162097842@qq.com>
2019-12-16 01:10:05 +0800

    调整格式

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
-if(version_compare(PHP_VERSION,'5.3.0','<'))
+if (version_compare(PHP_VERSION, '5.3.0', '<')) {
     die('require PHP > 5.3.0 !');
+}
 
-// THINK_PATH	框架目录
-// STORAGE_TYPE	存储类型（默认为File）
-// APP_MODE	应用模式（默认为common）
-define('BUILD_DIR_SECURE', false);          # don't generate index.html
-define('APP_DEBUG', TRUE);                  # debug mode
-define('APP_PATH','./Application/');        # apps dir, only one
-define('RUNTIME_PATH', '/tmp/tp-runtime/');        # runtime dir, need writable   
+// THINK_PATH    框架目录
+// STORAGE_TYPE    存储类型（默认为File）
+// APP_MODE    应用模式（默认为common）
+define('BUILD_DIR_SECURE', false); # don't generate index.html
+define('APP_DEBUG', true); # debug mode
+define('APP_PATH', './Application/'); # apps dir, only one
+define('RUNTIME_PATH', '/tmp/tp-runtime/'); # runtime dir, need writable
 require './ThinkPHP/ThinkPHP.php';
---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+// 1.
+// namespace My\App;
+
+// use Exception;
+
+// class Foo {
+//     public function doSomething() {
+//         throw new Exception("someting error");
+//     }
+// }
+
+// $a = new Foo();
+// try {
+//     $a -> doSomething();
+// } catch( Exception $e ) {
+//     var_dump($e);
+// }
+
