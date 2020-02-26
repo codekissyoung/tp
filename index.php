874e9fb2591ec13b59e953e077d9df3e70d55c94
codekissyoung <1162097842@qq.com>
2020-02-26 23:34:57 +0800

    update

--- index.php
+++ index.php
@@ -1,54 +1,5 @@
 <?php
 
-class emailer{
-    private $sender;
-    private $recipients;
-    private $subject;
-    private $body;
-
-    function __construct($sender)
-    {
-        $this -> sender = $sender;
-        $this -> recipients = [];
-    }
-
-    public function addRecipients($recipient){
-        array_push($this->recipients, $recipient);
-    }
-
-    public function setSubject($subject){
-        $this->subject = $subject;
-    }
-
-    public function setBody($body){
-        $this->body = $body;
-    }
-
-    public function sendEmail(){
-        foreach ($this->recipients as $recipient){
-            $result = mail(
-                $recipient,
-                $this->subject,
-                $this->body,
-                "From : {$this->sender}" );
-            if($result)
-                echo "邮件成功发送到：{$recipient}";
-        }
-    }
-}
-
-function __autoload( $className )
-{
-    echo $className;
-}
-// ------------------------ client code ---------------------------
-
-$emailer = new emailer("1162097842@qq.com");
-
-var_dump($emailer);
-
-//$obj = new MyClass();
-
 exit();
 // --------------------------- ThinkPHP ----------------------------------
 
