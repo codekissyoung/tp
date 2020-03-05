4504c181dec982099e59a591f04545799534df8f
codekissyoung <1162097842@qq.com>
2020-03-05 21:41:02 +0800

    upgrade

--- ThinkPHP/ThinkPHP.php
+++ ThinkPHP/ThinkPHP.php
@@ -1,15 +1,10 @@
 <?php
-$GLOBALS['_beginTime'] = microtime(true); // 记录开始运行时间
-//echo $GLOBALS['_beginTime'];
+$GLOBALS['_beginTime'] = microtime(true);
 define('MEMORY_LIMIT_ON', function_exists('memory_get_usage')); // 记录内存初始使用
-
-//echo MEMORY_LIMIT_ON;
-
 
 if (MEMORY_LIMIT_ON) {
     $GLOBALS['_startUseMems'] = memory_get_usage();
 }
-//exit();
 
 // 版本信息
 const THINK_VERSION = '3.2.3';
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 echo "hello";
 
-// --------------------------- ThinkPHP ----------------------------------
-define('APP_DEBUG', true); // debug mode
-define('APP_PATH', './Application/'); // apps dir, only one
-define('RUNTIME_PATH', '/tmp/tp-runtime/'); // runtime dir, need writable
+session_start();
+
+define('APP_PATH', './Application/');
+define('RUNTIME_PATH', '/tmp/tp-runtime/');
 require './ThinkPHP/ThinkPHP.php';
